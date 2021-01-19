832e3c89ef6ca7db3fec0a4cfa134f2085cff509
Maggie S <Maggiesaldivia@gmail.com>
2021-01-18 19:53:01 -0800

    Created a cipher.html page and a JS script. The function is supposed to switch the first and last letter of the response. Posted on Stack Overflow as function is not returning results: https://stackoverflow.com/questions/65785236/function-used-to-switch-first-and-last-letter-of-string-not-returning-results

---
+++ js/cipher-scripts.js
@@ -0,0 +1,9 @@
+function firstAndLast(response) {
+  firstLetter = response.charAt(str.length - 1)
+  lastLetter = response.charAt(0)
+  middle = response.substring(1,(response.length-1))
+  return firstLetter + middle + lastLetter
+  }
+
+const response = prompt("What did you do for dinner tonight?");
+alert(firstAndLast(response));
